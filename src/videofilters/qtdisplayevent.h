a50dc1a6571fd6564fef8857a3a469c9a3184b5b
Hong-She Liang <starofrainnight@gmail.com>
2015-10-21 14:00:31 +0800

    Added qt display event for display an image on QtDisplayWindow in future

---
+++ src/videofilters/qtdisplayevent.h
@@ -0,0 +1,37 @@
+/*
+Qt video display filter for mediastreamer2
+
+Copyright (C) 2015 Hong-She Liang
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+#ifndef INCLUDE_MEDIASTREAMER2_QTDISPLAYEVENT_H_INCLUDED_FCF355F077B511E58F2B00E04C68002D
+#define INCLUDE_MEDIASTREAMER2_QTDISPLAYEVENT_H_INCLUDED_FCF355F077B511E58F2B00E04C68002D
+
+#include <QEvent>
+
+class QtDisplayEvent : public QEvent
+{
+public:
+	QtDisplayEvent(Type type=(Type)Display);
+
+	/**
+	 * Display event id;
+	 */
+	static int Display;
+};
+
+#endif /* INCLUDE_MEDIASTREAMER2_QTDISPLAYEVENT_H_INCLUDED_FCF355F077B511E58F2B00E04C68002D */
