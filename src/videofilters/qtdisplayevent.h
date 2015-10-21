94f5de757741e34877d05853e48af1ba775aaab9
Hong-She Liang <starofrainnight@gmail.com>
2015-10-21 15:15:25 +0800

    Removed default value of QtDisplayEvent constructor, it must be clear input by user

--- src/videofilters/qtdisplayevent.h
+++ src/videofilters/qtdisplayevent.h
@@ -26,7 +26,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 class QtDisplayEvent : public QEvent
 {
 public:
-	QtDisplayEvent(Type type=(Type)Display);
+	QtDisplayEvent(Type type);
 
 	/**
 	 * Display event id;
